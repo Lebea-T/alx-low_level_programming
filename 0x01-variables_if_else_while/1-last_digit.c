4cc93ab131174f1e0bf4a8db6cd89ec44276d64d
Tshiphiwe <tshepewalebea15@gmail.com>
2023-09-09 10:26:42 -0700

    mxm

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,16 +16,16 @@ int main(void)
 
 	if (Last_digit > 5)
 	{
-		printf("Last digit of %d is %d, and greater than 5", n, Last_digit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, Last_digit);
 	}
 	else
 		if (Last_digit == 0)
 		{
-			printf("Last digit of %d is %d and is 0", n, Last_digit);
+			printf("Last digit of %d is %d and is 0\n", n, Last_digit);
 		}
 		else
 		{
-		printf("Last digit of %d is %d and is less than 6 and not 0", n, Last_digit);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, Last_digit);
 		}
 	return (0);
 }
