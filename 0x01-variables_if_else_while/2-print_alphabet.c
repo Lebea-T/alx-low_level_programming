6dd4105428b3407384dc4cf1528e6500ac18f13b
Tshiphiwe <tshepewalebea15@gmail.com>
2023-09-09 11:25:17 -0700

    i try hey

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - entry point
+ * description: this program prints alphbets in lowcase
+ * Return: always 0
+ */
+int main(void)
+{
+	char alphabets[] = "abcdefghijklmnopqrstuvwxyz";
+
+	int count = 0;
+
+	while (count < (int)strlen(alphabets))
+	{
+	putchar(alphabets[count]);
+	count++;
+	}
+	return (0);
+}
