3108f6f70efe58f65c3d32cb0c973faedf0bc5d5
Tshiphiwe <tshepewalebea15@gmail.com>
2023-09-19 13:28:52 -0700

    changing the value of n

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - puts character to standard output
+ * @c: character to put to standard output
+ */
+int _putchar(char c);
+
+/**
+ * reset_to_98: updates the value of n to 98
+ * n: an int value to be modified
+ * Return: nothing
+ */
+void reset_to_98(int *n);
+#endif
