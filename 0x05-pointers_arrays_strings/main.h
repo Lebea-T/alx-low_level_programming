280abc87a9761ab8ff20fcddce3ecbb9b58c9573
Tshiphiwe <tshepewalebea15@gmail.com>
2023-09-19 13:47:58 -0700

    swap on the go

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -13,4 +13,13 @@ int _putchar(char c);
  * Return: nothing
  */
 void reset_to_98(int *n);
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: integer to swap
+ * @b: integer to swap
+ */
+void swap_int(int *a, int *b);
+
+
 #endif
